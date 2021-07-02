ebf62072ab8d1b99f678f006866f46a08e4a8c8f
sl0wedzik <86803537+sl0wedzik@users.noreply.github.com>
2021-07-02 15:47:29 +0200

    Update file.js

--- file.js
+++ file.js
@@ -34,11 +34,34 @@ client.on("message", async message => {
 
     if (command == "cut") {
         let cut = new Discord.MessageEmbed()
-               .setColor(`#36393f`)
+            .setColor(`#36393f`)
             .setImage(`https://media1.tenor.com/images/bd030a7745e1ae72a6c27e13a23fbfdf/tenor.gif?itemid=14870978`)
         message.channel.send(cut)
+    }
+
+    if (command == "uptime") {
+        let uptime = new Discord.MessageEmbed()
+            .setColor(`#36393f`)
+            .setDescription(`**${prettyMilliseconds(client.uptime)}**`)
+            .setImage(`https://linearb.io/wp-content/uploads/2020/05/hero-1.gif`)
+        message.channel.send(uptime)
+    }
+
+    if (command == "home") {
+        let home = new Discord.MessageEmbed()
+            .setDescription(`**Welcome to our bot's home!**\n\n**!**cut (⋆⋆⋆/⋆⋆⋆⋆⋆)\n**!**uptime (⋆⋆/⋆⋆⋆⋆⋆)\n**!**home (⋆⋆⋆⋆/⋆⋆⋆⋆⋆)\n**!**wstęp (⋆⋆⋆⋆⋆/⋆⋆⋆⋆⋆)\n\n**The asterisks show the popularity of the command**`)
+            .setImage(`https://i.pinimg.com/originals/93/c7/44/93c744bcde1780c94bb1d3f03991f8a7.gif`)
+        message.channel.send(home)
+    }
+
+    if (command == "personel") {
+        let personel = new Discord.MessageEmbed()
+            .setColor(`#36393f`)
+            .setDescription(`soon...`)
+            .setImage(`https://monophy.com/media/sLlA3D82b344w/monophy.gif`)
+        message.channel.send(personel)
     }
     
 });
 
-client.login('ODYwMTI5MjcwMjUxMzg4OTYw.YN2vzg.JrK2zFK_tDUOU62DRwsSkV5Og0w');
+client.login('token');
